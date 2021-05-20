f2f9a576b01d9271a85ae2a09d387f3a6097ff69
ViniciusPieroniZanetiniVergilio <vinizanetini@gmail.com>
2021-05-20 01:23:47 -0300

    Site

---
+++ grupo_api/API/config/config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  // Insira aqui seus dados do banco NA NUVEM AZURE
+  production: {
+    // altere APENAS username, password, database e host.
+    username: 'lettutech',
+    password: 'cristopher_123',
+    database: 'database-iot',
+    host: 'serv-gp2-lettutech.database.windows.net',
+    dialect: 'mssql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    },
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
+  },
+
+  // Insira aqui seus dados do banco LOCAL - MySQL Workbench
+  dev: {
+    // altere APENAS username, password e database.
+    username: 'root',
+    password: '123456789',
+    database: 'cad',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    },
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
+  },
+};
---
+++ grupo_api/API/models/usuario.js
@@ -0,0 +1,40 @@
+	'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    let Usuario = sequelize.define('Usuario',{
+		id_cliente: {
+			field: 'id_cliente',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},		
+		nome: {
+			field: 'nome',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		email: {
+			field: 'email',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		senha: {
+			field: 'senha',
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	}, 
+	{
+		tableName: 'cliente', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return Usuario;
+};
